5ff4ff263388def611f99c7a07aeacbac4355d22
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-07-02 02:23:38 +0900

    Remove unneccesarry primary key

--- data/migrations/20170702020421_create_dependency.up.sql
+++ data/migrations/20170702020421_create_dependency.up.sql
@@ -2,5 +2,4 @@ create table dependency (
     project_uuid foreign key not null references projects(uuid),
     dependent_project_uuid foreign key not null references projects(uuid),
     source_cd smallint not null default 0
-    primary key(project_uuid, dependent_project_uuid)
 );
