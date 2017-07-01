3cb087fab4cc09d0363b8117429d9d8e8e2adc9c
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-07-02 02:30:29 +0900

    Fix unique indexing

--- data/migrations/20170702012901_create_user.up.sql
+++ data/migrations/20170702012901_create_user.up.sql
@@ -1,6 +1,9 @@
 create table user (
     id serial primary key,
+    
     user_name varchar(255),
-    github_uuid varchar(255) UNIQUE,
+    github_uuid varchar(255),
     github_access_token varchar(255)
 );
+
+crete unique index index_user_on_github_uuid on user (github_uuid);
--- data/migrations/20170702015658_create_project.up.sql
+++ data/migrations/20170702015658_create_project.up.sql
@@ -5,7 +5,9 @@ create table project (
     pods_file_content text,
     xcode_xml_content text,
     supported_swift_version varchar(255),
-    repository varchar(255) unique,
+    repository varchar(255),
     last_fetched_at timestamp without time zone,
     state_cd smallint default 0 not null
 );
+
+create uique index index_project_on_repository on project (repository);
