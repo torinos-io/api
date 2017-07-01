bf2ded276c818aafc8e5bceab271e5707f5d23be
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-07-02 03:14:49 +0900

    Insert description for source cd

--- type/model/dependency.go
+++ type/model/dependency.go
@@ -1,8 +1,14 @@
 package model
 
+// SourceCD
+// 0 = unknown
+// 1 = carthage
+// 2 = cocoapods
+// 3 = submodule
+
 // Dependency has project dependency and source distination.
 type Dependency struct {
 	ProjectUUID          string `json:"project_uuid"`
 	DependentProjectUUID string `json:"dependent_project_uuid"`
-	SourceCD             string `json:"source_cd"`
+	SourceCD             int    `json:"source_cd"`
 }
