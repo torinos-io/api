a46ff0567e5344e00b46d6989a58560cda3d2114
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-07-02 02:16:02 +0900

    Fix composite primary key

--- data/migrations/20170702020421_create_dependency.up.sql
+++ data/migrations/20170702020421_create_dependency.up.sql
@@ -1,5 +1,6 @@
 create table dependency(
-    project_uuid primary key foreign key not null references projects(uuid),
-    dependent_project_uuid primary key foreign key not null references projects(uuid),
+    project_uuid foreign key not null references projects(uuid),
+    dependent_project_uuid foreign key not null references projects(uuid),
     source_cd smallint not null default 0
+    primary key(project_uuid, dependent_project_uuid)
 );
