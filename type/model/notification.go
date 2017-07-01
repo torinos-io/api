e3b68d4fc5fa0712337a77dd1d7e665ba803cbbe
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-07-01 23:48:27 +0900

    Add notification model

---
+++ type/model/notification.go
@@ -0,0 +1,10 @@
+package model
+
+// Notification represents user's notification settings.
+type Notification struct {
+	ID          int    `json:"id"`
+	UserID      int    `json:"user_id"`
+	ProjectUUID string `json:"project_uuid"`
+	Email       string `json:"email"`
+	DeletedAt   Time   `json:"deleted_at"`
+}
