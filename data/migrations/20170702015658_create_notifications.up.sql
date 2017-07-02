8ee92b7934555037075f88dbdb89cb7072b1e021
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-07-02 18:38:23 +0900

    Tweak dependency migraion file

--- data/migrations/20170702015658_create_notifications.up.sql
+++ data/migrations/20170702015658_create_notifications.up.sql
@@ -1,7 +1,7 @@
 create table notifications (
     id serial primary key,
     user_id int references users(id),
-    project_uuid varchar(255) not null references projects(uuid),
+    project_uuid uuid not null references projects(uuid),
     email varchar(255) not null,
     deleted_at timestamp without time zone
 );
--- data/migrations/20170702020421_create_dependencies.up.sql
+++ data/migrations/20170702020421_create_dependencies.up.sql
@@ -1,7 +1,7 @@
 create table dependencies (
-    project_uuid varchar(255) not null references projects(uuid),
-    dependent_project_uuid varchar(255) not null references projects(uuid),
+    project_uuid uuid not null references projects(uuid),
+    dependent_project_uuid uuid not null references projects(uuid),
     source_cd smallint not null default 0
 );
 
-create index index_dependencies_on_project_uuid_and_dependent_project_uuid on (project_uuid, dependent_project_uuid);
+create index index_dependencies_on_project_uuid_and_dependent_project_uuid on dependencies (project_uuid, dependent_project_uuid);
