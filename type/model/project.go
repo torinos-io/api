069bfc9ba20196ecf37295b27dbdb8e966d8a729
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-07-02 18:18:16 +0900

    Tweak projects scheme difinition

--- type/model/project.go
+++ type/model/project.go
@@ -12,8 +12,8 @@ type Project struct {
 	UUID                  string `json:"uuid"`
 	UserID                int    `json:"user_id"`
 	CartFileContent       string `json:"cart_file_content"`
-	PodsFileContent       string `json:"pods_file_content"`
-	XcodeXMLContent       string `json:"xcode_xml_content"`
+	PodFileLockContent    string `json:"pods_file_content"`
+	PBXproject            string `json:"xcode_xml_content"`
 	SupportedSwiftVersion string `json:"supported_swift_version"`
 	Repository            string `json:"repository"`
 	LastFetchedAt         Time   `json:"last_fetched_at"`
