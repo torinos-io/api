14669d5abe7b3087ce5dfeea6f9ccdb8d24d5f6a
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-07-08 15:55:51 +0900

    Tweak migrations

--- data/migrations/20170702020421_create_dependencies.up.sql
+++ data/migrations/20170702020421_create_dependencies.up.sql
@@ -1,8 +1,8 @@
 create table dependencies (
-    source_cd smallint not null default 0,
-
     project_uuid            uuid not null references projects(uuid),
-    dependent_project_uuid  uuid not null references projects(uuid)
+    dependent_project_uuid  uuid not null references projects(uuid),
+
+    source_cd smallint not null default 0
 );
 
 create index index_dependencies_on_project_uuid_and_dependent_project_uuid on dependencies (project_uuid, dependent_project_uuid);
