73165df1679770344a4f126ed3d5eef077dc1daa
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-07-16 16:10:44 +0900

    Implement user store

--- store/user_store/store.go
+++ store/user_store/store.go
@@ -2,6 +2,9 @@ package store
 
 import (
 	"github.com/jinzhu/gorm"
+	"github.com/go-errors/errors"
+
+	"github.com/torinos-io/api/type/model"
 )
 
 type concreteStore struct {
@@ -10,6 +13,8 @@ type concreteStore struct {
 
 // Store is an interface for CRUD category records
 type Store interface {
+	SaveGithubAccount(uuid string, accessToken string) (*model.User, error)
+	FindByGithubAccount(uuid string) (*model.User, error)
 }
 
 // New creates a store
@@ -17,4 +22,35 @@ func New(db *gorm.DB) Store {
 	return &concreteStore{
 		db: db,
 	}
+}
+
+func (s *concreteStore) SaveGithubAccount(uuid string, accessToken string) (*model.User, error) {
+	user := &model.User{}
+	finder := s.db.Where("github_uuid = ?", uuid).Find(user)
+
+	if err := finder.Error; err != nil && err != gorm.ErrRecordNotFound {
+		return user, errors.Wrap(err, 0)
+	}
+
+	user.GithubAccessToken = accessToken
+
+	db := s.db.Save(user)
+
+	if err := db.Error; err != nil {
+		return user, errors.Wrap(err, 0)
+	}
+
+	return user, nil
+}
+
+
+func (s *concreteStore) FindByGithubAccount(uuid string) (*model.User, error) {
+	user := &model.User{}
+	finder := s.db.Where("github_uuid = ?", uuid).Find(user)
+
+	if err := finder.Error; err != nil {
+		return user, errors.Wrap(err, 0)
+	}
+
+	return user, nil
 }
