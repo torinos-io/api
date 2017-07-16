3b0ff712b0e030f040d6ce70c3e1a956ec52458c
Yuki Yamada <yamadayuuki0818@gmail.com>
2017-07-17 02:30:46 +0900

    Fix to receive http.Handler

--- server/util/cors_handler.go
+++ server/util/cors_handler.go
@@ -3,12 +3,11 @@ package util
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"github.com/rs/cors"
 )
 
 // NewCorsHandler creates a new http.Handler to support CORS
-func NewCorsHandler(engine *gin.Engine, allowOrigins []string) http.Handler {
+func NewCorsHandler(handler http.Handler, allowOrigins []string) http.Handler {
 	mw := cors.New(cors.Options{
 		AllowedOrigins:   allowOrigins,
 		AllowCredentials: true,
@@ -17,5 +16,5 @@ func NewCorsHandler(engine *gin.Engine, allowOrigins []string) http.Handler {
 		Debug:            false,
 	})
 
-	return mw.Handler(engine)
+	return mw.Handler(handler)
 }
