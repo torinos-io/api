2ec46e98aee6122f629401bb40c06c1a5f3556a7
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-07-23 22:24:40 +0900

    Set foreign key information to Subscription

--- type/model/subscription.go
+++ type/model/subscription.go
@@ -6,9 +6,9 @@ import (
 
 // Subscription represents user's notification settings.
 type Subscription struct {
-	ID          int      `json:"id"`
-	UserID      null.Int `json:"user_id"`
-	ProjectUUID string   `json:"project_uuid"`
+	ID          int      `json:"id" gorm:"primary_key"`
+	UserID      null.Int `json:"user_id" gorm:"ForeignKey:ID"`
+	ProjectUUID string   `json:"project_uuid" gorm:"ForeignKey:UUID"`
 
 	Email     string `json:"email"`
 	DeletedAt Time   `json:"deleted_at"`
