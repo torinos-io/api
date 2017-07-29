1ba82a41583a02133aef355d2d25022f73dae6a3
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-07-29 14:28:03 +0900

    Add seed data sql

---
+++ seed_projects.sql
@@ -0,0 +1,4 @@
+INSERT INTO projects VALUES ('15dc4443-b8ea-4a34-9dda-b18533267bb4', 1, '', '', '', '2.3', 'awsome-ios-app', DEFAULT, 0),('fcfdeb4f-15d6-4bfd-9ca7-397476756feb', 2, '', '', '', '3.0', 'brilliant-ios-app', DEFAULT, 0),
+('96ae7c15-8513-46b9-9160-99029e4054a8', 3, '', '', '', '3.0', 'splendid-ios-app', DEFAULT, 0),
+('7cd1a30d-38fe-4444-9a1e-42372e765ebe', null, '', '', '', '2.3', 'happy-ios-app', DEFAULT, 0),
+('6dea063e-4d2d-4094-9ec1-eef4b739c7b6', 4, '', '', '', '2.3', 'super-ios-app', DEFAULT, 0);
---
+++ seed_users.sql
@@ -0,0 +1,1 @@
+INSERT INTO users VALUES (1, '1245678901', 'user01', '', 'user01@example.com'),(2, '1245678902', 'user02', '', 'user02@example.com'),(3, '1245678903', 'user03', '', 'user03@example.com'),(4, '1245678904', 'user04', '', 'user04@example.com');
