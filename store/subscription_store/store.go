aaa390683787859773a9f6e61987611ebd1ca3d6
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-07-29 15:26:57 +0900

    Save subscription

--- store/subscription_store/store.go
+++ store/subscription_store/store.go
@@ -1,6 +1,14 @@
 package store
 
-import "github.com/jinzhu/gorm"
+import (
+	"regexp"
+
+	"github.com/go-errors/errors"
+	"github.com/guregu/null"
+	"github.com/jinzhu/gorm"
+
+	"github.com/torinos-io/api/type/model"
+)
 
 type concreteStore struct {
 	db *gorm.DB
@@ -8,6 +16,8 @@ type concreteStore struct {
 
 // Store is an interface for CRUD category records
 type Store interface {
+	CreateSubscription(user *model.User, projectUUID string) (*model.Subscription, error)
+	DeleteSubscription() error
 }
 
 // New creates a store
@@ -15,4 +25,40 @@ func New(db *gorm.DB) Store {
 	return &concreteStore{
 		db: db,
 	}
+}
+
+func (s *concreteStore) CreateSubscription(user *model.User, projectUUID string) (*model.Subscription, error) {
+	subscription := &model.Subscription{}
+
+	finder := s.db.
+		Where("user_id = ?", user.ID).
+		Where("project_uuid", projectUUID).
+		Find(subscription)
+
+	if err := finder.Error; err != nil && err != gorm.ErrRecordNotFound {
+		return subscription, errors.Wrap(err, 0)
+	}
+
+	emailPattern := regexp.MustCompile(`^(?i:[^ @"<>]+|".*")@(?i:[a-z1-9.])+.(?i:[a-z])+$`)
+
+	if !emailPattern.MatchString(user.Email) {
+		return subscription, errors.New("Invalid email")
+	}
+
+	subscription.UserID = null.IntFrom(int64(user.ID))
+	subscription.Email = user.Email
+	subscription.DeletedAt = nil
+	subscription.ProjectUUID = projectUUID
+
+	db := s.db.Save(subscription)
+
+	if err := db.Error; err != nil {
+		return subscription, errors.Wrap(err, 0)
+	}
+
+	return subscription, nil
+}
+
+func (s *concreteStore) DeleteSubscription() error {
+	return nil
 }
