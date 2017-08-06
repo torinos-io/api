58603ef6d0249228af4cf842eb520348268211ed
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-08-06 15:25:35 +0900

    Format

--- server/route/subscription.go
+++ server/route/subscription.go
@@ -2,13 +2,13 @@ package route
 
 import (
 	"net/http"
-	
+
 	"github.com/gin-gonic/gin"
-	
+
+	"github.com/go-errors/errors"
 	"github.com/torinos-io/api/server/middleware"
 	subscription_service "github.com/torinos-io/api/service/subscription_service"
 	subscription_store "github.com/torinos-io/api/store/subscription_store"
-	"github.com/go-errors/errors"
 )
 
 // Subscribe subscription
@@ -19,23 +19,23 @@ func Subscribe(c *gin.Context) {
 		Config:            ac.Config,
 		SubscriptionStore: subscriptionStore,
 	})
-	
+
 	req := &subscription_service.SubscribeRequest{}
-	
+
 	if err := middleware.BindJSON(c, req); err != nil {
 		c.Error(errors.Wrap(err, 0))
 		return
 	}
-	
+
 	user := middleware.GetCurrentUser(c)
-	
+
 	subscription, err := service.Subscribe(req, user)
-	
+
 	if err != nil {
 		c.Error(err)
 		return
 	}
-	
+
 	c.JSON(http.StatusOK, subscription)
 }
 
@@ -47,18 +47,18 @@ func Unsubscribe(c *gin.Context) {
 		Config:            ac.Config,
 		SubscriptionStore: subscriptionStore,
 	})
-	
+
 	req := &subscription_service.UnSubscribeRequest{}
 	if err := middleware.BindJSON(c, req); err != nil {
 		c.Error(errors.Wrap(err, 0))
 		return
 	}
 	user := middleware.GetCurrentUser(c)
-	
+
 	if err := service.UnSubscribe(req, user); err != nil {
 		c.Error(err)
 		return
 	}
-	
+
 	c.Status(http.StatusOK)
 }
