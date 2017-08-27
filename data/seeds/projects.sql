fd6ef7f5cd66e7b3ddb5ede2cfac9f4fdb0f5c5d
Yuki Iwanaga <yuki@creasty.com>
2017-08-27 20:58:23 +0900

    Fix order, oops

--- data/seeds/projects.sql
+++ data/seeds/projects.sql
@@ -6,13 +6,13 @@ declare
   delta_uuid uuid   := '88dafdad-d158-4b19-bcf8-7daca5b23481';
   epsilon_uuid uuid := '5a964485-d9f4-4080-afd5-0cd90f80b832';
   zeta_uuid uuid    := '70992e6b-ce49-47ce-b52c-8f69e28c578e';
+  eta_uuid uuid     := 'd325ef73-7eab-4c8b-8333-28bb0187a467';
+  theta_uuid uuid   := '572dd0d7-32d5-4234-a7f7-42933c3d4b5a';
+  iota_uuid uuid    := '082941d4-f5f3-4e2e-b37d-ea7b4b346d87';
   phi_uuid uuid     := 'cad4335d-0ca3-4d79-aa10-6a168b46e1d0';
   chi_uuid uuid     := 'f680ebdd-678e-4c74-b05b-045fb0e20e1e';
   psi_uuid uuid     := 'a30035d6-c9e2-4376-8773-954384830509';
   omega_uuid uuid   := 'f2419558-940a-47e1-aefa-075e27a078b3';
-  eta_uuid uuid     := 'd325ef73-7eab-4c8b-8333-28bb0187a467';
-  theta_uuid uuid   := '572dd0d7-32d5-4234-a7f7-42933c3d4b5a';
-  iota_uuid uuid    := '082941d4-f5f3-4e2e-b37d-ea7b4b346d87';
 begin
 
   insert into projects
